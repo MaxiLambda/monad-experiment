dd635f44abcd578ea52901da418441e968d4d73e
Maxi <maximilian.lincks@gmx.net>
2024-10-15 17:42:30 +0200

    split foldMap

--- src/main/java/lincks/maximilian/foldable/Foldable.java
+++ src/main/java/lincks/maximilian/foldable/Foldable.java
@@ -1,5 +1,6 @@
 package lincks.maximilian.foldable;
 
+import lincks.maximilian.alternative.Alternative;
 import lincks.maximilian.monadpluszero.MonadPlusZero;
 import lincks.maximilian.util.Bottom;
 
@@ -9,14 +10,20 @@ import java.util.function.Function;
 import static lincks.maximilian.monadzero.MonadZero.zero;
 
 public interface Foldable<F extends Foldable<F, ?>, T> extends Bottom<F,T> {
-    //with Monoid instead of MonadPlus, this would be more powerful
+    //with Monoid instead of MonadPlusZero/Alternative, foldMap would be more powerful
     //monoids need to be implemented with classes providing a mempty and mconcat operation
     //maybe use instances instead of generic classes
 
-    default <M extends MonadPlusZero<M, R>, R> MonadPlusZero<M, R> foldMap(Function<T, MonadPlusZero<M, R>> f, Class<M> clazz) {
-        return foldr((T t, MonadPlusZero<M, R> acc) -> (MonadPlusZero<M, R>) acc.mplus(f.apply(t)), zero(clazz));
+    /** foldMap implementation based on {@link Alternative}. */
+    default <A extends Alternative<A, R>, R> Alternative<A, R> foldMapA(Function<T, Alternative<A, R>> f, Class<A> clazz) {
+        return foldr((T t, Alternative<A, R> acc) -> acc.alternative(() -> f.apply(t)), zero(clazz));
     }
 
-    //without Monoids, this probably can't be expressed as foldMap
+    /** foldMap implementation based on {@link MonadPlusZero} */
+    default <M extends MonadPlusZero<M, R>, R> MonadPlusZero<M, R> foldMapM(Function<T, MonadPlusZero<M, R>> f, Class<M> clazz) {
+        return foldr((T t, MonadPlusZero<M, R> acc) -> acc.alternative(() -> f.apply(t)), zero(clazz));
+    }
+
+    //without Monoids, this probably can't be expressed as foldMap/ or at lest not in a useful way
     <R> R foldr(BiFunction<T, R, R> acc, R identity);
 }
