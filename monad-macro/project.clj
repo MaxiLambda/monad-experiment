1e46b4ef06092037a2f6e1817971dab0987a3c2d
Maxi <maximilian.lincks@gmx.net>
2024-11-23 00:20:12 +0100

    versioning

--- monad-macro/project.clj
+++ monad-macro/project.clj
@@ -3,5 +3,5 @@
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url  "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.12.0"]
-                 [lincks.maximilian.monads/monad-experiment "1.0-SNAPSHOT"]]
+                 [lincks.maximilian.monads/monad-experiment "1.0.0"]]
   )
